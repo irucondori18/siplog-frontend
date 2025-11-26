b78fdd7a00c83ab54608f612f838cfca01c5f832
41035026 <41035026@fi.unju.edu.ar>
2025-11-25 23:46:04 -0300

    Se sube la creacion de tabla

--- src/app/features/proveedor/pages/proveedor-list/proveedor-list.component.ts
+++ src/app/features/proveedor/pages/proveedor-list/proveedor-list.component.ts
@@ -1,34 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProveedorService } from '../../data-access/proveedor.service';
 import { Proveedor } from '../../data-access/proveedor.model';
+import { CommonModule } from '@angular/common'; // <-- Importa esto
 
 @Component({
   selector: 'app-proveedor-list',
-  imports: [],
+  standalone: true, // <-- Si es un componente standalone
+  imports: [CommonModule], //aqui tambien se agrego
   templateUrl: './proveedor-list.component.html',
   styleUrl: './proveedor-list.component.scss'
 })
-export class ProveedorListComponent {
+export class ProveedorListComponent implements OnInit{
+  //lista de provedores
+  listaProveedores: Proveedor[] = [];
 
-  proveedores: Proveedor[] = [];
-
-  constructor(private proveedorService: ProveedorService) {}
+  constructor(private proveedorService: ProveedorService) { }
 
   ngOnInit(): void {
-    this.cargarProveedores();
+    this.cargarProveedores2();
   }
 
-  cargarProveedores(): void {
-    this.proveedorService.getProveedores().subscribe({
-      next: (data) => {
-        this.proveedores = data;
-        console.log('Proveedores cargados:', this.proveedores);
-        //asignarlo a una tabla, create camioneros, un alta de transportistas, de transporte
-        //cambiando
-      },
-      error: (err) => {
-        console.error('Error al cargar proveedores', err);
-      },
+  cargarProveedores2(): void {
+    this.proveedorService.getProveedores().subscribe(response => {
+      console.log(response);
+      this.listaProveedores=response as Proveedor[];
     });
   }
 }
+
+//  cargarProveedores(): void {
+//    this.proveedorService.getProveedores().subscribe({
+//      next: (data) => {
+//        this.proveedores = data;
+//        console.log('Proveedores cargados:', this.proveedores);
+//asignarlo a una tabla, create camioneros, un alta de transportistas, de transporte
+//cambiando
+//      },
+//      error: (err) => {
+//        console.error('Error al cargar proveedores', err);
+//      },
+//    });
+//  }
+//}
